11531830dd11fbb7edf07f5fdb38cdad3238cabf
Kirandeep Kaur <kian16.ak@gmail.com>
2020-08-13 08:53:51 +0530

    Add files via upload

---
+++ gcd_fast.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int gcd_fast(int a, int b) {
+    int max,min,t;
+    if(a>=b)
+    {
+        max = a;
+        min = b;
+    }
+    else
+    {
+        max=b;
+        min=a;
+    }
+    while(min!=0)
+    {
+        t=min;
+        min = max%min;
+        max=t;
+    }
+    return max;
+
+
+
+}
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << gcd_fast(a, b) << std::endl;
+  return 0;
+}
